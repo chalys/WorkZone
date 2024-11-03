63658f8b845699efcbde94ed45597feeef802ed2
Carlos Roberto Arando Llanos <chalys.all@gmail.com>
2024-11-03 19:43:21 -0400

    Updates

--- routes/other.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { home } = require('../controllers/other');
-
-router.get('/', home)
-router.get('/home', home)
-
-module.exports = router;
