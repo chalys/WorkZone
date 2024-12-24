1bd269af8ba365e3f5ee9bf076e7bfcf060ee7da
Carlos Roberto Arando Llanos <chalys.all@gmail.com>
2024-12-24 16:48:01 -0400

    chore(JIRA-15): add product JSON and update images on the home page

--- src/controllers/other/home.controller.js
+++ src/controllers/other/home.controller.js
@@ -1,4 +1,7 @@
+const products = require("../../data/json/products.json")
 module.exports = (req,res)=>{
-    res.render("./other/home")
+    res.render("./other/home",{
+        products
+    })
 
 }
